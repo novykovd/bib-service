536f44d6da8c3f5525e30b8ed2bc296cd82669c4
D <novykov.sqrt@gmail.com>
2024-07-06 15:57:23 +0200

    rest api to return user details + new isbn table for list of books

---
+++ src/main/java/com/example/demo/ApiController.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    private static final Logger logger = LoggerFactory.getLogger(ApiController.class);
+
+    @GetMapping("/userDetails")
+    public Customer getUser() throws Exception{
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username;
+        try{
+            username = ((UserDetails) principal).getUsername();
+            return customerRepository.findByUsername(username);
+        }catch(Exception e){
+            logger.info("something went wrong");
+            throw e;
+        }
+    }
+}
--- src/main/java/com/example/demo/Customer.java
+++ src/main/java/com/example/demo/Customer.java
@@ -2,15 +2,30 @@ package com.example.demo;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String username;
     private String password;
 
+
+    @ElementCollection
+    @CollectionTable(name = "isbn_collection", joinColumns =  @JoinColumn(name = "entity_id"))
+    @Column(name = "isbn")
+    private List<Long> isbnList;
+
     protected Customer(){}
+
+    public Customer(String username, String password, List<Long> isbnList){
+        this.password = password;
+        this.username = username;
+        this.isbnList = isbnList;
+    }
 
     public Customer(String username, String password){
         this.password = password;
@@ -23,8 +38,10 @@ public class Customer {
     public String getPassword(){
         return password;
     }
-
+    public List<Long> getIsbnList() { return isbnList;}
     public Long getId() {
         return id;
     }
+
+
 }
